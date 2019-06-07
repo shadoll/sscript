9cbc5c37b87421c40e9b7ebf86114c4e17687f2f
sHa <dev@shadoll.work>
2019-06-07 10:52:51 +0300

    scripts update

--- scripts/git.storage_push.sh
+++ scripts/git.storage_push.sh
@@ -8,7 +8,22 @@
 #
 
 DIR=$(dirname $BASH_SOURCE)
-PROJECT=$(cd $DIR"/.."; pwd)
+PROJECT=(${DIR//node_modules/ })
+
+if [ ! -d $PROJECT/storage ]; then
+    echo "ERROR: '$PROJECT/storage' dir not found"
+    exit 1
+fi
+
+gity --version 2>&1 >/dev/null
+GIT_IS_AVAILABLE=$?
+if [ ! $GIT_IS_AVAILABLE -eq 0 ]; then
+    echo "ERROR: The program 'git' is currently not installed."
+    echo "Try: 'sudo apt install git' to install it"
+    exit 1
+fi
+
+
 cd $PROJECT/storage
 
 git add . --all &&
