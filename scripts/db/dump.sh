b15c5dc53b4ba13eb07f85254c446fb55d7007f6
sHa <sha@shadoll.dev>
2019-07-10 08:43:44 +0300

    Update

--- scripts/db/dump.sh
+++ scripts/db/dump.sh
@@ -3,14 +3,21 @@
 # @package   sscripts
 # @author    sHa <sha@shadoll.dev>
 # @copyright 2019 shadoll
-# @version   19.6.7
+# @version   19.7.10
 # @link      https://shadoll.dev
 #
 
-DIR=$(dirname "$0")
-ROOT=$(cd "$DIR"/.. && pwd)
+DIR=$(dirname $BASH_SOURCE)
+PROJECT=(${DIR//node_modules/ })
 
-DATABASE=$(awk -F'=' '/^MYSQL_DATABASE/ { print $2}' $ROOT/docker/config.env)
-PWD=$(awk -F'=' '/^MYSQL_ROOT_PASSWORD/ { print $2}' $ROOT/docker/config.env)
+if [ ! -f $PROJECT/docker-compose.yml ]; then
+    echo "ERROR: '$PROJECT/docker-compose.yml' file not found"
+    exit 1
+fi
+
+cd $PROJECT
+
+DATABASE=$(awk -F'=' '/^MYSQL_DATABASE/ { print $2}' $PROJECT/docker/config.env)
+PWD=$(awk -F'=' '/^MYSQL_ROOT_PASSWORD/ { print $2}' $PROJECT/docker/config.env)
 
 docker-compose exec db /usr/bin/mysqldump --skip-comments -uroot --password=$PWD $DATABASE > $ROOT/storage/database/dump/database.sql 2>/dev/null
