651ac79d1cc5769c02455fac11b63b99f0847b30
sHa <sha@shadoll.dev>
2019-07-11 10:49:28 +0300

    Update

--- scripts/db/dump.sh
+++ scripts/db/dump.sh
@@ -3,7 +3,7 @@
 # @package   sscripts
 # @author    sHa <sha@shadoll.dev>
 # @copyright 2019 shadoll
-# @version   19.7.10
+# @version   19.7.11
 # @link      https://shadoll.dev
 #
 
@@ -20,4 +20,4 @@ cd $PROJECT
 DATABASE=$(awk -F'=' '/^MYSQL_DATABASE/ { print $2}' $PROJECT/docker/config.env)
 PWD=$(awk -F'=' '/^MYSQL_ROOT_PASSWORD/ { print $2}' $PROJECT/docker/config.env)
 
-docker-compose exec db /usr/bin/mysqldump --skip-comments -uroot --password=$PWD $DATABASE > $ROOT/storage/database/dump/database.sql 2>/dev/null
+docker-compose exec db /usr/bin/mysqldump --skip-comments -uroot --password=$PWD $DATABASE > $PROJECT/storage/database/dump/database.sql 2>/dev/null
